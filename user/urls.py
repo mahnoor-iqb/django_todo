ae0efd40f83b7a7412e79eded4407de54f236915
mahnoor-iqb <mahnoor.iqbal@emumba.com>
2022-06-16 15:43:15 +0500

    Modify user urls

--- user/urls.py
+++ user/urls.py
@@ -9,8 +9,8 @@ from user.views import (
 )
 
 urlpatterns = [
-    path('', AdminView.as_view(), name="admin_api"),
-    path('delete/<int:user_id>', AdminDetailView.as_view(), name="admin_delete_api"),
+    path('admin', AdminView.as_view(), name="admin_api"),
+    path('admin/<int:user_id>', AdminDetailView.as_view(), name="admin_delete_api"),
     path('<int:user_id>', UserDetailView.as_view(), name="user_detail_api"),
     path('signup', SignupView.as_view(), name="signup"),
     path('login', LoginView.as_view(), name="login"),
